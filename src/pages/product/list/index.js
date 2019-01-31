b23a1efe2a7edfb3cafd1122db41f9fb5e087279
Andi Sultan <andisultan@payfazz>
2019-01-31 14:08:25 +0700

    update page product

--- src/pages/product/list/index.js
+++ src/pages/product/list/index.js
@@ -1,21 +1,83 @@
 import React, { Component } from 'react'
-import { Card, Select, Table, message } from 'antd'
+import { Card, Select, Table, Tag } from 'antd'
 import { withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import TableControl from '../../../components/table-control'
 import Filter from '../../../components/filter'
-import { columns } from './column'
 import { ProductApi, ProviderApi } from '../../../api'
-import { OPTIONS_CONFIG_ACTIVE, OPTIONS_CONFIG_PROBLEM } from '../../../config/options'
-import { inputMoneyHandler } from '../../../utils/formatter/currency'
-import { getError } from '../../../utils/error/api'
+import { OPTIONS_CONFIG_ACTIVE } from '../../../config/options'
 import { parseUrlQueryParams, compareParams, generateUrlQueryParams } from '../../../utils/url-query-params'
 import MENU from '../../../config/menu'
 import { PRODUCT_TABS } from '../../../config/product'
 import { hasAccess } from '../../../utils/roles'
 import { ROLES_ITEMS } from '../../../config/roles'
 import './product-list.scss'
+
+const dataSource = [
+  {
+    key: '1',
+    product_name_code: 'Telkomsel 5000 [TLKM5]',
+    product_description: 'Pulsa reguler Rp. 5000',
+    price: 'Rp. 4.850',
+    status: 'Active',
+  },
+  {
+    key: '2',
+    product_name_code: 'Telkomsel 5000 [TLKM5]',
+    product_description: 'Pulsa reguler Rp. 5000',
+    price: 'Rp. 4.850',
+    status: 'Inactive',
+  },
+  {
+    key: '3',
+    product_name_code: 'Telkomsel 5000 [TLKM5]',
+    product_description: 'Pulsa reguler Rp. 5000',
+    price: 'Rp. 4.850',
+    status: 'Inactive',
+  },
+  {
+    key: '4',
+    product_name_code: 'Telkomsel 5000 [TLKM5]',
+    product_description: 'Pulsa reguler Rp. 5000',
+    price: 'Rp. 4.850',
+    status: 'Active',
+  },
+  {
+    key: '5',
+    product_name_code: 'Telkomsel 5000 [TLKM5]',
+    product_description: 'Pulsa reguler Rp. 5000',
+    price: 'Rp. 4.850',
+    status: 'Active',
+  },
+];
+
+const columns = [
+  {
+    title: 'Product Name & Code',
+    dataIndex: 'product_name_code',
+    key: 'product_name_code',
+  }, 
+  {
+    title: 'Product Description',
+    dataIndex: 'product_description',
+    key: 'product_description',
+  }, 
+  {
+    title: 'Price',
+    dataIndex: 'price',
+    key: 'price',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+    render: tag => {
+      let color = tag === 'Inactive' ? '#f16a70' : '#a1cd43'
+      return <Tag color={color} key={tag}>{tag}</Tag>
+    }
+  }
+];
 
 class ProductList extends Component {
   constructor(props) {
@@ -33,10 +95,6 @@ class ProductList extends Component {
       hasAccessEdit: hasAccess(ROLES_ITEMS.PRODUCT_UPDATE),
       hasAccessEditFields: {
         status: hasAccess(ROLES_ITEMS.PRODUCT_EDIT_STATUS),
-        problem: hasAccess(ROLES_ITEMS.PRODUCT_EDIT_PROBLEM),
-        description: hasAccess(ROLES_ITEMS.PRODUCT_EDIT_DESCRIPTION),
-        price: hasAccess(ROLES_ITEMS.PRODUCT_EDIT_PRICE),
-        provider: hasAccess(ROLES_ITEMS.PRODUCT_EDIT_PROVIDER),
       },
     }
   }
@@ -60,32 +118,6 @@ class ProductList extends Component {
     this.addUrlQueryParamsAndUpdateData({ ...params, page: 1, [field]: value })
   }
 
-  changeSwitch = (checked, idx, field) => {
-    const { data } = this.state
-    data[idx][field] = checked
-    data[idx].isEdited = true
-    this.setState({ ...this.state, data })
-  }
-
-  changeDescription = (e, idx) => {
-    const { data } = this.state
-    data[idx].description = e.target.value
-    this.setState({ ...this.state, data })
-  }
-
-  changePrice = (e, idx) => {
-    const { data } = this.state
-    data[idx].adminPrice = inputMoneyHandler(e.target.value)
-    this.setState({ ...this.state, data })
-  }
-
-  changeProvider = (e, idx) => {
-    const { data } = this.state
-    data[idx].providerId = e.target.value
-    data[idx].isEdited = true
-    this.setState({ ...this.state, data })
-  }
-
   changeSearch = (e) => {
     const { value } = e.target
     const { params } = this.state
@@ -94,47 +126,6 @@ class ProductList extends Component {
     } else {
       this.addUrlQueryParamsAndUpdateData({ ...params, page: 1, searchQuery: '' })
     }
-  }
-
-  clickEdit = (idx, field) => {
-    const { data } = this.state
-    data[idx][field] = true
-    data[idx].isEdited = true
-    this.setState({ ...this.state, data })
-  }
-
-  clickSave = async (record) => {
-    const { data } = this.state
-    const { id, idx, name, description, adminPrice, providerId, problem, active } = record
-    const payload = {
-      description,
-      adminPrice,
-      providerId,
-      problem,
-      active,
-    }
-    data[idx].loading = true
-    await this.setState({ ...this.state, data })
-    await ProductApi.update(id, payload)
-      .then(() => {
-        message.success(`Update ${name} success`)
-        data[idx].isEdited = false
-        data[idx].isEditDesc = false
-        data[idx].isEditPrice = false
-        data[idx].loading = false
-        this.setState({ ...this.state, data })
-      })
-      .catch((err) => {
-        data[idx].loading = false
-        this.setState({ ...this.state, data })
-        message.error(getError(err) || `Update ${name} failed`)
-      })
-  }
-
-  clickCancel = (idx) => {
-    const { data, originalData } = this.state
-    data[idx] = { ...originalData[idx] }
-    this.setState({ ...this.state, data })
   }
 
   getData = async (params = this.state.params) => {
@@ -196,39 +187,15 @@ class ProductList extends Component {
   }
 
   render() {
-    const { loading, data, params, valPerPage, providers, hasAccessEdit, hasAccessEditFields } = this.state
+    const { loading, params, valPerPage } = this.state
     const leftFilter = (
       <div className="flex">
         <div style={{ marginRight: '1em' }}>
-          <label className="small-text">Provider:</label>
-          <div>
-            <Select value={params.providerId || ''} style={{ width: 150 }} onChange={e => this.changeFilter(e, 'providerId')}>
-              {
-                providers.map(provider => (
-                  <Select.Option key={provider.id} value={provider.id}>{provider.name}</Select.Option>
-                ))
-              }
-            </Select>
-          </div>
-        </div>
-        <div style={{ marginRight: '1em' }}>
-          <label className="small-text">Active:</label>
+          <label className="small-text">Product Status:</label>
           <div>
             <Select value={params.active || ''} style={{ width: 150 }} onChange={e => this.changeFilter(e, 'active')}>
               {
                 OPTIONS_CONFIG_ACTIVE.map(option => (
-                  <Select.Option key={option.id} value={option.id}>{option.name}</Select.Option>
-                ))
-              }
-            </Select>
-          </div>
-        </div>
-        <div>
-          <label className="small-text">Problem:</label>
-          <div>
-            <Select value={params.problem || ''} style={{ width: 150 }} onChange={e => this.changeFilter(e, 'problem')}>
-              {
-                OPTIONS_CONFIG_PROBLEM.map(option => (
                   <Select.Option key={option.id} value={option.id}>{option.name}</Select.Option>
                 ))
               }
@@ -252,23 +219,12 @@ class ProductList extends Component {
         />
         <Filter left={leftFilter} />
         <Card>
-          <Table
-            className="table-responsive --big"
-            loading={loading}
-            rowKey="id"
-            dataSource={data}
-            columns={columns(
-              this.changeSwitch,
-              this.clickEdit,
-              this.clickCancel,
-              this.changeDescription,
-              this.changePrice,
-              this.changeProvider,
-              this.clickSave,
-              hasAccessEdit,
-              hasAccessEditFields,
-            )}
-            pagination={false}
+          <Table 
+            className="table-responsive --big" 
+            loading={loading} 
+            dataSource={dataSource} 
+            columns={columns} 
+            pagination={false} 
           />
         </Card>
       </div>
