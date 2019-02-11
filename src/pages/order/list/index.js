79c6c8c251f213bf6e777306a735e26fb75cc193
Syafrizal Akhzan <rizal@payfazz.com>
2019-02-11 13:55:45 +0700

    delete filter in order list

--- src/pages/order/list/index.js
+++ src/pages/order/list/index.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react'
-import { Card, Modal, Select, Table, message } from 'antd'
+import { Card, Modal, Table, message } from 'antd'
 import { withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import TableControl from '../../../components/table-control'
-import Filter from '../../../components/filter'
 import { columns } from './column'
-import { OrderApi, ClientApi, ProviderApi } from '../../../api'
+import { OrderApi } from '../../../api'
 import MENU from '../../../config/menu'
 import ModalStatus from '../modal'
-import { OPTIONS_CONFIG_ORDER_STATUS, FILTER_ORDER_STATUS } from '../../../config/options'
 import { getError } from '../../../utils/error/api'
 import { ORDER_STATUS } from '../../../config/order'
 import { generateUrlQueryParams, parseUrlQueryParams, compareParams } from '../../../utils/url-query-params'
-import Role from '../../../components/role'
-import { ROLES_ITEMS } from '../../../config/roles'
-import { hasAccess } from '../../../utils/roles'
 import OrderReport from '../report'
 import './order-list.scss'
 
@@ -33,15 +28,12 @@ class OrderList extends Component {
       modal: false,
       modalData: {},
       selected: {},
-      providers: [],
-      clients: [],
     }
   }
 
   componentDidMount() {
     const { location } = this.props
     this.getData(parseUrlQueryParams(location.search))
-    this.getFilterData()
   }
 
   componentDidUpdate(prevProps) {
@@ -144,21 +136,6 @@ class OrderList extends Component {
       })
   }
 
-  getFilterData = () => {
-    Promise.all([
-      ClientApi.getAll(),
-      ProviderApi.getAll(),
-    ])
-      .then((res) => {
-        const clients = res[0].data.clients || []
-        clients.unshift({ id: '', name: 'All Clients' })
-        const providers = res[1].data.providers || []
-        providers.unshift({ id: '', name: 'All Providers' })
-        this.setState({ ...this.state, clients, providers })
-      })
-      .catch(() => {})
-  }
-
   search = (searchValue) => {
     const { params } = this.state
     this.addUrlQueryParamsAndUpdateData({ ...params, page: 1, query: searchValue })
@@ -191,67 +168,7 @@ class OrderList extends Component {
   }
 
   render() {
-    const { loading, data, params, valPerPage, modal, modalData, clients, providers, total } = this.state
-    // const leftFilter = (
-    //   <div className="flex">
-    //     <Role roleItem={ROLES_ITEMS.ORDER_FILTER_PROVIDER} style={{ marginRight: '1em' }}>
-    //       <div>
-    //         <label className="small-text">Provider:</label>
-    //         <div>
-    //           <Select value={params.providerId || ''} style={{ width: 150 }} onChange={e => this.changeFilter(e, 'providerId')}>
-    //             {
-    //               providers.map(provider => (
-    //                 <Select.Option key={provider.id} value={provider.id.toString()}>{provider.name}</Select.Option>
-    //               ))
-    //             }
-    //           </Select>
-    //         </div>
-    //       </div>
-    //     </Role>
-    //     <Role roleItem={ROLES_ITEMS.ORDER_FILTER_CLIENT} style={{ marginRight: '1em' }}>
-    //       <label className="small-text">Client:</label>
-    //       <div>
-    //         <Select value={params.clientId || ''} style={{ width: 150 }} onChange={e => this.changeFilter(e, 'clientId')}>
-    //           {
-    //             clients.map(client => (
-    //               <Select.Option key={client.id} value={client.id.toString()}>{client.name}</Select.Option>
-    //             ))
-    //           }
-    //         </Select>
-    //       </div>
-    //     </Role>
-    //     <Role roleItem={ROLES_ITEMS.ORDER_FILTER_STATUS}>
-    //       <label className="small-text">Order Status:</label>
-    //       <div>
-    //         <Select value={params.filterType || ''} style={{ width: 150 }} onChange={e => this.changeFilter(e, 'filterType')}>
-    //           {
-    //             OPTIONS_CONFIG_ORDER_STATUS.map(status => (
-    //               <Select.Option key={status.id} value={status.id}>{status.name}</Select.Option>
-    //             ))
-    //           }
-    //         </Select>
-    //       </div>
-    //     </Role>
-    //   </div>
-    // )
-    // const rightFilter = (
-    //   <div className="flex">
-    //     <div
-    //       className={`order-list__summary-item ${(loading || !hasAccess(ROLES_ITEMS.ORDER_FILTER_STATUS)) ? '--disabled' : ''}`}
-    //       onClick={loading || !hasAccess(ROLES_ITEMS.ORDER_FILTER_STATUS) ? () => {} : () => this.clickFilterStatus(FILTER_ORDER_STATUS.PENDING)}
-    //     >
-    //       <div className="order-list__summary-item-value --pending">{total.pending || 0}</div>
-    //       <div>PENDING</div>
-    //     </div>
-    //     <div
-    //       className={`order-list__summary-item ${(loading || !hasAccess(ROLES_ITEMS.ORDER_FILTER_STATUS)) && '--disabled'}`}
-    //       onClick={loading || !hasAccess(ROLES_ITEMS.ORDER_FILTER_STATUS) ? () => {} : () => this.clickFilterStatus(FILTER_ORDER_STATUS.ERROR)}
-    //     >
-    //       <div className="order-list__summary-item-value --error">{total.failed || 0}</div>
-    //       <div>ERROR</div>
-    //     </div>
-    //   </div>
-    // )
+    const { loading, data, params, valPerPage, modal, modalData } = this.state
     return (
       <div>
         <TableControl
@@ -266,7 +183,6 @@ class OrderList extends Component {
           changeSearch={this.changeSearch}
         />
         <OrderReport />
-        {/* <Filter left={leftFilter} right={rightFilter} /> */}
         <Card>
           <Table
             className="table-responsive"
