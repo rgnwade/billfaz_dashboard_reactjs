e72e2a77a57028a4d0a251389d5d20df545e4e35
andrepangestu <andre@payfazz.com>
2019-02-14 19:57:45 +0700

    design popover profile

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,46 @@
 import React from 'react'
-import { Layout } from 'antd'
+import { Layout, Popover, Button, Avatar, Divider } from 'antd'
 import PropTypes from 'prop-types'
 
 import Logo from '../../assets/billfazz-logo-29.png'
 import './header.scss'
+
+
+// const logout = () => {
+//   UserApi.logout()
+//     .then(() => BACK_TO_LOGIN())
+//     .catch(err => message.error(getErrorMessage(err) || 'Logout failed. Please try again'))
+// }
+
+const content = (
+  <div className="popover-profile">
+    <div>
+      <Avatar style={{ backgroundColor: '#a1cd43' }} size="large" >
+        AB
+      </Avatar>
+    </div>
+    <div>
+      Albert Burgess
+    </div>
+    <div>
+      Admin
+    </div>
+    <Divider/>
+    <div>
+      Main Email Address
+    </div>
+    <div>
+      albert@payfazz.com
+    </div>
+    <Divider/>
+    <div>
+      My Profile
+    </div>
+    <div>
+      Sign Out
+    </div>
+  </div>
+);
 
 const { Header } = Layout
 const AppHeader = ({ toggle, title, backToHome }) => (
@@ -19,6 +56,18 @@ const AppHeader = ({ toggle, title, backToHome }) => (
       </div>
       <div className="app-header__title">
         {title}
+      </div>
+      <div className="app-header__popover">
+        <Avatar style={{ backgroundColor: '#a1cd43', verticalAlign: 'middle' }} size="large">
+          AB
+        </Avatar>
+        <Popover placement="bottomRight" content={content} trigger="click">
+          <div>
+            <Button className="btn-name_profile">
+              Albert Burgess
+            </Button>
+          </div>
+        </Popover>
       </div>
     </div>
   </Header>
