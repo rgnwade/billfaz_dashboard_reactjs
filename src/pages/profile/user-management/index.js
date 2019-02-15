2758c545d3c544a0eaad73b04e4695f80da64032
Syafrizal Akhzan <rizal@payfazz.com>
2019-02-15 18:01:56 +0700

    add dummy table in profile - user management

--- src/pages/profile/user-management/index.js
+++ src/pages/profile/user-management/index.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react'
+import { Table } from 'antd'
+
+import TableControl from '../../../components/table-control'
 
 class UserManagement extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      data: {},
+      data: [],
       loading: false,
+      params: {},
+      valPerPage: 0,
     }
   }
 
   render() {
-    const { data, loading } = this.state
+    const { data, loading, params, valPerPage } = this.state
+    const columns = [{
+      title: 'Username',
+      dataIndex: 'name',
+      key: 'name',
+    }, {
+      title: 'Role',
+      dataIndex: 'age',
+      key: 'age',
+    }, {
+      title: '',
+      dataIndex: 'address',
+      key: 'address',
+    }]
     return (
       <div>
-        <h2>User Management</h2>
-        <div>{data.toString()} {loading.toString()}</div>
+        <h2 style={{ marginBottom: 0 }}>User Management</h2>
+        <TableControl
+          disableSearch
+          valPage={params.page}
+          valPerPage={valPerPage}
+          handlePrevPage={this.handlePrevPage}
+          handleNextPage={this.handleNextPage}
+          loading={loading}
+        />
+        <Table
+          className="table-responsive"
+          loading={loading}
+          rowKey="orderId"
+          dataSource={data}
+          columns={columns}
+          pagination={false}
+        />
       </div>
     )
   }
